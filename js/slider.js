f2679afbea50f5ab89e3ceb5bc8eb07d14a9924f
Bogdan <bohdan.ulich@gmail.com>
2022-05-03 23:23:44 +0300

    fix

--- js/slider.js
+++ js/slider.js
@@ -3,4 +3,14 @@ $(document).ready(function () {
         arrows: false,
         dots: true,
     })
+
+    $(".header__search").click(function () {
+        $(".header__search-input").addClass("active")
+    })
+    $(document).on("click", function (e) {
+        if (!$(e.target).closest(".header__search").length) {
+            $(".header__search-input").removeClass("active")
+        }
+        e.stopPropagation()
+    })
 })
